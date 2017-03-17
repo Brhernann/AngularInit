b3f974c4dbb2fed2b03b06aadec42993f51424b4
Brhernann <brhernann@gmail.com>
2017-03-16 21:25:59 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,11 +4,7 @@ var app = express();
 // Para llamar los archivos css y js públicos desde tu index.html debes
 // declarar las rutas como estáticas de la siguiente forma
 app.use(express.static('public'));
-app.use('/css', express.static(__dirname + '/css'));
-app.use('/js', express.static(__dirname + '/js'));
-app.use('/img', express.static(__dirname + '/img'));
-app.use('/views', express.static(__dirname + '/views'));
-app.use('/node_modules', express.static(__dirname + '/node_modules'));
+
 
 
 app.get('/',function(request,res){
